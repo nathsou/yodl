58286148be03a8f1ba63d44a6c5fa3710fa0d324
Nathan Soufflet <nathsoufflet@gmail.com>
2026-01-04 16:49:33 +0100

    Update gen_bitstream.sh

--- gen_bitstream.sh
+++ gen_bitstream.sh
@@ -6,14 +6,14 @@ set -e # Exit on error
 
 rm -f output/$module.json output/$module.asc output/$module.bin
 
-# firtool --format=fir -O=release --verilog \
-#     -disable-all-randomization -strip-debug-info \
-#     --lowering-options=disallowPackedArrays,disallowLocalVariables,emitBindComments \
-#     output/$module.fir -o output/$module.sv
+firtool --format=fir -O=release --verilog \
+    -disable-all-randomization -strip-debug-info \
+    --lowering-options=disallowPackedArrays,disallowLocalVariables,emitBindComments \
+    output/$module.fir -o output/$module.sv
 
-# yosys -p "verilog_defines -DENABLE_INITIAL_MEM_=1; read_verilog -sv output/$module.sv; check -assert; synth_ice40 -top Top -json output/$module.json"
+yosys -p "verilog_defines -DENABLE_INITIAL_MEM_=1; read_verilog -sv output/$module.sv; check -assert; synth_ice40 -top Top -json output/$module.json"
 
-yosys -p "verilog_defines -DENABLE_INITIAL_MEM_=1; read_rtlil output/$module.rtlil; check -assert; synth_ice40 -top Top -json output/$module.json"
+# yosys -p "verilog_defines -DENABLE_INITIAL_MEM_=1; read_rtlil output/$module.rtlil; check -assert; synth_ice40 -top Top -json output/$module.json"
 
 if [ $device == "cu" ]; then
     nextpnr-ice40 --hx8k --json output/$module.json --pcf examples/constraints/cu.pcf --package cb132 --freq 100 --asc output/$module.asc
